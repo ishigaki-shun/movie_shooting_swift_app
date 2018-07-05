8825d0917ec4a988465d13254a777cf6fadec118
mht-rentaro-matsushita <r.matsushita@mht-code.com>
2018-07-05 16:49:25 +0900

    first commit

---
+++ movie_shooting_swift_app/movie_shooting_swift_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  movie_shooting_swift_app
+//
+//  Created by 松下廉太郎 on 2018/07/05.
+//  Copyright © 2018年 manhattan.code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
