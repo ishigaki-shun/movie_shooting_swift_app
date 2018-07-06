15a8bfe8023566c46c07b8351f3c37673c569fa4
mht-rentaro-matsushita <r.matsushita@mht-code.com>
2018-07-06 12:13:07 +0900

    動画撮影機能追加

--- movie_shooting_swift_app/movie_shooting_swift_app/ViewController.swift
+++ movie_shooting_swift_app/movie_shooting_swift_app/ViewController.swift
@@ -6,20 +6,101 @@
 //  Copyright © 2018年 manhattan.code. All rights reserved.
 //
 
+
 import UIKit
+import AVFoundation
+import Photos
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
+    
+    
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var footerView: UIView!
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    var isRecoding = false
+    
+    var fileOutput: AVCaptureMovieFileOutput?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // セッションのインスタンス生成
+        let captureSession = AVCaptureSession()
+        
+        // 入力（背面カメラ）
+        let videoDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        let videoInput = try! AVCaptureDeviceInput(device: videoDevice!)
+        captureSession.addInput(videoInput)
+        // 入力（マイク）
+        let audioDevice = AVCaptureDevice.default(for: AVMediaType.audio)
+        let audioInput = try! AVCaptureDeviceInput.init(device: audioDevice!)
+        captureSession.addInput(audioInput);
+        // 出力（動画ファイル）
+        fileOutput = AVCaptureMovieFileOutput()
+        captureSession.canAddOutput(fileOutput!)
+        captureSession.addOutput(fileOutput!)
+        // プレビュー
+        let videoLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoLayer.frame = previewView.bounds
+        videoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewView.layer.addSublayer(videoLayer)
+
+        // セッションの開始
+        DispatchQueue.global(qos: .userInitiated).async {
+            captureSession.startRunning()
+        }
+        screenInitialization()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // 録画の開始・停止ボタン
+    @IBAction func tapStartStopButton(_ sender: Any) {
+        if isRecoding { // 録画終了
+            fileOutput?.stopRecording()
+        }
+        else{ // 録画開始
+            let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+            let documentsDirectory = paths[0] as String
+            let filePath : String? = "\(documentsDirectory)/temp.mp4"
+            let fileURL : NSURL = NSURL(fileURLWithPath: filePath!)
+            fileOutput?.startRecording(to: (fileURL as URL?)!, recordingDelegate: self as AVCaptureFileOutputRecordingDelegate)
+        }
+        isRecoding = !isRecoding
+        screenInitialization()
     }
-
-
+    
+    // 録画完了
+//    public func capture(_ captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAt outputFileURL: URL!, fromConnections connections: [Any]!, error: Error!) {
+//        // ライブラリへの保存
+//        PHPhotoLibrary.shared().performChanges({
+//            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL)
+//        }) { completed, error in
+//            if completed {
+//                print("Video is saved!")
+//            }
+//        }
+//    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        print("start")
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        // ライブラリへの保存
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL)
+        }) { completed, error in
+            if completed {
+                print("Video is saved!")
+            }
+        }
+    }
+    
+    func screenInitialization(){
+        startStopButton.setImage(UIImage(named: isRecoding ? "startButton" : "stopButton"), for: .normal)
+        headerView.alpha = isRecoding ? 0.6 : 1.0
+        footerView.alpha = isRecoding ? 0.6 : 1.0
+    }
 }
-
